373cf44588d0df37b3ad91e315bc2f5930f96675
Yoshida Norikatsu <norikatsu@gmail.com>
2024-05-01 21:37:51 +0900

    create_user_command move to Inner of setup()
    setup()必要化のため create_user_command を setup()内部に移動

--- lua/img_viewer.lua
+++ lua/img_viewer.lua
@@ -25,39 +25,29 @@ M.setup = function(args)
     --M.config = vim.tbl_deep_extend("force", M.config, args or {})
 
 
-    --# ---------- Sample api 
-    --# M.hello = function()
-    --#     print("Hello at lua...")
-    --# end
+    ---------- Sample api 
+    M.hello = function()
+        print("Hello at lua...")
+    end
 
-    --# ---------- nvim-tree api 
-    --# M.tree_open = function()
-    --#     tree_api.tree.toggle()
-    --# end
+    ---------- nvim-tree api 
+    M.tree_open = function()
+        tree_api.tree.toggle()
+    end
 
-    --# M.tree_get = function()
-    --#     file_name = tree_api.tree.get_node_under_cursor()
-    --#     print( file_name )
-    --# end
+    M.tree_get = function()
+        file_name = tree_api.tree.get_node_under_cursor()
+        print( file_name )
+    end
 
+    ---------- User Command
+    vim.api.nvim_create_user_command("MyHello", require("img_viewer").hello, {})
+    vim.api.nvim_create_user_command("MyOpen",  require("img_viewer").tree_open, {})
+    vim.api.nvim_create_user_command("MyGet",   require("img_viewer").tree_get, {})
 end
 
 
 
----------- Sample api 
-M.hello = function()
-    print("Hello at lua...")
-end
-
----------- nvim-tree api 
-M.tree_open = function()
-    tree_api.tree.toggle()
-end
-
-M.tree_get = function()
-    file_name = tree_api.tree.get_node_under_cursor()
-    print( file_name )
-end
 
 return M
 
--- plugin/img_viewer.lua
+++ plugin/img_viewer.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_create_user_command("MyHello", require("img_viewer").hello, {})
-vim.api.nvim_create_user_command("MyOpen",  require("img_viewer").tree_open, {})
-vim.api.nvim_create_user_command("MyGet",   require("img_viewer").tree_get, {})
+---------- Move to Inner of setup()
+
+-- vim.api.nvim_create_user_command("MyHello", require("img_viewer").hello, {})
+-- vim.api.nvim_create_user_command("MyOpen",  require("img_viewer").tree_open, {})
+-- vim.api.nvim_create_user_command("MyGet",   require("img_viewer").tree_get, {})
 
