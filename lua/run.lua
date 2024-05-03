da11d81a579425fdadcd15839cf7b646622ecc3f
Yoshida Norikatsu <norikatsu@gmail.com>
2024-05-03 17:52:23 +0900

    module キャッシュ削除のパス修正
    キャッシュ削除時のパスを / に指定していると、require() 時のパス指定も / にしないと削除された状態で動作しないため
    
    おそらく / と . でキャッシュが別管理になっている

--- lua/run.lua
+++ lua/run.lua
@@ -2,19 +2,22 @@
 
 -- キャッシュされたモジュールを削除
 package.loaded['img_viewer'] = nil
-package.loaded['img_viewer/module'] = nil
+package.loaded['img_viewer.module'] = nil
 
 -- モジュールを呼び出す
 local img_viewer = require('img_viewer')
-
+local module     = require("img_viewer.module")
 
 
 -- 関数呼び出し
 img_viewer.setup() -- setup() しないと使えないように作成した場合には必須
 
+
 -- 確認したい関数
 --img_viewer.hello()
-img_viewer.tree_open()
+--img_viewer.tree_open()
 --img_viewer.tree_get()
 
+img_viewer.api_test()
+--module.is_wezterm_preview_open()
 
